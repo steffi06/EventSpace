36637ffa2d2cb93e0a653196fa7d2f7151b8bb69
Stephanie Shupe <stephanie.shupe@lookout.com>
2013-03-30 11:44:44 -0700

    initial commit

---
+++ EventSpace/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EventSpace
+//
+//  Created by Stephanie Shupe on 3/30/13.
+//  Copyright (c) 2013 EventSpace, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
